2175907a9457ad1ccd8d2262b03673ff54932ccf
Cerek Hillen <cerekh@gmail.com>
2023-04-28 08:58:16 -0700

    normalize names

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+fn normalize(name: impl AsRef<str>) -> String {
+    // https://peps.python.org/pep-0503/#normalized-names
+    name.as_ref()
+        .to_lowercase()
+        .replace("_", "-")
+        .replace(".", "-")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
